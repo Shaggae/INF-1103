68d77097e1ed0a195cbee324fa689ca29909a5a1
Shaggae <t.umathorn@gmail.com>
2026-09-04 16:56:44 +0800

    follower growth tracker

--- hello.py
+++ hello.py
@@ -7,6 +7,15 @@ username = "Cool_creator"
 bio = "Fun Blogger"
 follower = 100
 
+follower += 50
+print("Day 1:", follower)
+
+follower += 20
+print("Day 2:", follower)
+
+follower -= 10
+print("Day 3:", follower)
+
 print("Username: ", username)
 print("Bio:", bio)
 print("Followers:", follower)
