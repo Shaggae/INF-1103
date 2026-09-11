cd195c91a211dfdac840685437bdd7aef6f43abf
Shaggae <t.umathorn@gmail.com>
2026-09-11 15:39:53 +0800

    Reporting

--- Tutorial 2/auditor.py
+++ Tutorial 2/auditor.py
@@ -1,19 +1,25 @@
 inventory = 0
+rejected = 0
 while True:
     stock = input("Enter stock values: ")
-    if stock != "quit":
-        if stock.isdigit():
+    if stock.lower() != "quit":
+        try:
+            stock = int(stock)
             if stock > 0:
+                inventory += stock
                 if inventory < 500:
-                    inventory += stock
                     continue
                 else:
-                    print("The stock exceeds 500 units.")
+                    print("The stock exceeds 500 units")
                     break
             else:
-                print("Please input a positive integer: ")
-        else:
-            print("Please input an integer: ")
+                print("Please input a positive integer")
+                rejected += 1
+        except ValueError:
+            print("Please input an integer")
+            rejected += 1
     else:
+        print("Total Units Processed: " + str(inventory))
+        print("Number of failed entries: " + str(rejected))
         break
 
