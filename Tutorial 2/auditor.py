11814ab5a4318e8ac0293d6eb32b6fe81ceb92e4
Shaggae <t.umathorn@gmail.com>
2026-09-11 14:40:15 +0800

    Validation check for stock

--- Tutorial 2/auditor.py
+++ Tutorial 2/auditor.py
@@ -1,6 +1,19 @@
 inventory = 0
 while True:
     stock = input("Enter stock values: ")
-
-
+    if stock != "quit":
+        if stock.isdigit():
+            if stock > 0:
+                if inventory < 500:
+                    inventory += stock
+                    continue
+                else:
+                    print("The stock exceeds 500 units.")
+                    break
+            else:
+                print("Please input a positive integer: ")
+        else:
+            print("Please input an integer: ")
+    else:
+        break
 
