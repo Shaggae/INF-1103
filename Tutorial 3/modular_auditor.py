b063faf883998e56b1797201adb1b9954cf32fba
Shaggae <t.umathorn@gmail.com>
2026-09-18 15:39:12 +0800

    main function

--- Tutorial 3/modular_auditor.py
+++ Tutorial 3/modular_auditor.py
@@ -28,3 +28,33 @@ def generate_report(total_units, failed_attempts):
     print(f"Number of Failed/Rejected Entries: {failed_attempts}")
     print("-------------------------")
 
+def main():
+    inventory = 0
+    deliveries_processed = 0
+    rejected = 0
+    total_tax_collected = 0
+
+    while True:
+        result = get_valid_input()
+
+        if result == "quit":
+            generate_report(deliveries_processed, rejected)
+            print(f"Final Inventory Total: {inventory}")
+            print(f"Total Tax Collected: {total_tax_collected:.2f}")
+            break
+
+        elif result == "invalid":
+            rejected += 1
+            continue
+
+        else:
+            inventory = process_delivery(inventory, result)
+            tax = calculate_tax(result)
+            total_tax_collected += tax
+            deliveries_processed += 1
+
+            print(f"Delivery of {result} units recorded. "
+                  f"Tax: {tax:.2f} | Running Inventory Total: {inventory}")
+
+if __name__ == "__main__":
+    main()
