4d337209824d3bc51714259a83264e945a35f7b7
Shaggae <t.umathorn@gmail.com>
2026-09-25 14:14:04 +0800

    display_inventory()

--- Tutorial 4/persistent_auditor.py
+++ Tutorial 4/persistent_auditor.py
@@ -5,30 +5,35 @@ INVENTORY_FILE = "inventory.txt"
 
 def load_inventory(filename=INVENTORY_FILE):
     if not os.path.exists(filename):
-        print(f"'{filename}' not found — starting with no existing orders.")
+        print(f"'{filename}' not found — starting with an empty inventory.")
         return []
-
-    orders = []
+    
+    inventory = []
     try:
         with open(filename, "r", newline="") as f:
             reader = csv.reader(f)
             for row in reader:
                 if not row:
                     continue
-                order_id, product, quantity = row
-                orders.append({
-                    "id": int(order_id.strip()),
+                item_id, product, quantity = row
+                inventory.append({
+                    "id": int(item_id.strip()),
                     "product": product.strip(),
                     "quantity": int(quantity.strip())
                 })
-        return orders
+        return inventory
     except (ValueError, IndexError):
         print(f"'{filename}' contained invalid data — starting fresh.")
         return []
 
-def save_inventory(orders, filename=INVENTORY_FILE):
+def save_inventory(inventory, filename=INVENTORY_FILE):
     with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
-        for order in orders:
-            writer.writerow([order["id"], f' {order["product"]}', f' {order["quantity"]}'])
+        for item in inventory:
+            writer.writerow([item["id"], f' {item["product"]}', f' {item["quantity"]}'])
 
+def display_inventory(inventory):
+    print("Current inventory:\n")
+    for item in inventory:
+        print(f'{item["id"]}, {item["product"]}, {item["quantity"]}')
+    print()
